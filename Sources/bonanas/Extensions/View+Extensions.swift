f265104f76dbddc2281ea6e29a20dc4729947dc8
Christopher Weems <hello@christopherweems>
2021-01-01 21:07:40 -0500

    Add `View.frame(minSize:idealSize:maxSide:alignment:)

--- Sources/bonanas/Extensions/View+Extensions.swift
+++ Sources/bonanas/Extensions/View+Extensions.swift
@@ -4,12 +4,24 @@
 import SwiftUI
 import unstandard
 
+
 // MARK: - Platform specific modifiers
 
 public extension View {
     func modifier<Result>(platform platforms: Platform..., modifications: (Self) -> Result) -> AnyView where Result : View {
         guard platforms.union().contains(.current) else { return AnyView(erasing: self) }
         return AnyView(erasing: modifications(self))
+    }
+    
+}
+
+// MARK: - Size
+
+public extension View {
+    func frame(minSize: CGSize? = nil, idealSize: CGSize? = nil, maxSize: CGSize? = nil, alignment: Alignment = .center) -> some View {
+        self.frame(minWidth: minSize?.width, idealWidth: idealSize?.width, maxWidth: maxSize?.width,
+                   minHeight: minSize?.height, idealHeight: idealSize?.height, maxHeight: maxSize?.height,
+                   alignment: alignment)
     }
     
 }
