ec668c1ee89b681c1a0b3fd19f64dc34d9a14725
Christopher Weems <hello@christopherweems>
2021-01-25 15:40:14 -0500

    Add `View.onScenePhaseChange(to:_:)

---
+++ Sources/bonanas/View Modifiers/ScenePhaseChangeCallbackViewModifier.swift
@@ -0,0 +1,41 @@
+//
+//  ScenePhaseChangeCallbackViewModifier.swift
+//  
+//
+//  Created by Christopher Weems on 1/25/21.
+//
+
+import SwiftUI
+
+fileprivate struct ScenePhaseChangeCallbackViewModifier: ViewModifier {
+    @Environment(\.scenePhase) var scenePhase
+    
+    let callbackScenePhase: ScenePhase
+    let action: () -> Void
+    
+    func body(content: Content) -> some View {
+        content
+            .onChange(of: scenePhase == callbackScenePhase) {
+                guard $0 else { return }
+                action()
+            }
+    }
+    
+    init(scenePhase: ScenePhase, perform action: @escaping () -> Void) {
+        self.callbackScenePhase = scenePhase
+        self.action = action
+        
+    }
+    
+}
+
+public extension View {
+    func onScenePhaseChange(to value: ScenePhase, _ update: @escaping () -> Void) -> some View {
+        self.modifier(ScenePhaseChangeCallbackViewModifier(scenePhase: value, perform: update))
+            .onAppear {
+                guard value == .active else { return }
+                update()
+            }
+    }
+    
+}
