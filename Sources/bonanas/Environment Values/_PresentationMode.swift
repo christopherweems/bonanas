6bfed83b7f057cc1434d34b3bb471b6e273457fa
Christopher Weems <hello@christopherweems>
2021-01-25 20:03:40 -0500

    Add `EnvironmentValues._presentationMode`

---
+++ Sources/bonanas/Environment Values/_PresentationMode.swift
@@ -0,0 +1,40 @@
+//
+//  _PresentationMode.swift
+//  
+//
+//  Created by Christopher Weems on 1/25/21.
+//
+
+import SwiftUI
+
+public struct _PresentationMode {
+    public internal(set) var isPresented = true
+    internal var dismissCallback: () -> Void = { }
+    
+    public mutating func dismiss() {
+        guard isPresented else { return }
+        dismissCallback()
+        isPresented = false
+        
+    }
+    
+}
+
+public extension EnvironmentValues {
+    var _presentationMode: Binding<_PresentationMode> {
+        self[_PresentationModeEnvironmentKey.self]
+    }
+    
+    internal var __presentationMode: Binding<_PresentationMode> {
+        get { self[_PresentationModeEnvironmentKey.self] }
+        set { self[_PresentationModeEnvironmentKey.self] = newValue }
+    }
+    
+}
+
+fileprivate enum _PresentationModeEnvironmentKey: EnvironmentKey {
+    static var defaultValue: Binding<_PresentationMode> {
+        .constant(.init(isPresented: false))
+    }
+    
+}
