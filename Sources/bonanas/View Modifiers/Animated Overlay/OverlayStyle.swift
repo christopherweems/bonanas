1a43b65d2a9442fc29c392f80d83c05dcaa9e82a
Christopher Weems <hello@christopherweems>
2021-01-25 22:55:03 -0500

    Add `OverlayStyle`

---
+++ Sources/bonanas/View Modifiers/Animated Overlay/OverlayStyle.swift
@@ -0,0 +1,12 @@
+//
+//  OverlayStyle.swift
+//  
+//
+//  Created by Christopher Weems on 1/25/21.
+//
+
+public enum OverlayStyle {
+    case blurredBackground
+//        case revealBehind
+    
+}
