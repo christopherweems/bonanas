0fb2db5fa7c480a5a87d5ebf3d2ad47cc48c38d6
Christopher Weems <hello@christopherweems>
2021-01-28 22:18:02 -0500

    Add `Image.init(_:)`

---
+++ Sources/bonanas/Extensions/Image+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Image+Extensions.swift
+//  
+//
+//  Created by Christopher Weems on 1/28/21.
+//
+
+import SwiftUI
+
+extension Image {
+    public init(_ other: Image) {
+        self = other
+    }
+
+}
