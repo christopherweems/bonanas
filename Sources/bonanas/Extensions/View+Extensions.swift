6c61fd5da041620f7aadff9da81c424684c717ec
Christopher Weems <hello@christopherweems>
2021-01-31 21:09:17 -0500

    Add `alignment` argument to @ViewBuilder `View.overlay(alignment:_:)`

--- Sources/bonanas/Extensions/View+Extensions.swift
+++ Sources/bonanas/Extensions/View+Extensions.swift
@@ -43,8 +43,8 @@ public extension View {
 // MARK: - View Builder Overlay
 
 public extension View {
-    func overlay<Overlay>(@ViewBuilder _ overlay: () -> Overlay) -> some View where Overlay: View {
-        self.overlay(overlay())
+    func overlay<Overlay>(alignment: Alignment = .center, @ViewBuilder _ overlay: () -> Overlay) -> some View where Overlay: View {
+        self.overlay(overlay(), alignment: alignment)
     }
     
 }
