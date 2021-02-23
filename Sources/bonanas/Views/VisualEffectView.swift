5dbbf7bb910b1e50f56133f39c04c74e70e2d93a
Christopher Weems <hello@christopherweems>
2021-02-23 16:26:58 -0500

    Add `VisualEffectView`

---
+++ Sources/bonanas/Views/VisualEffectView.swift
@@ -0,0 +1,29 @@
+//  VisualEffectView.swift
+//  Created by Christopher Weems on 2/23/21
+
+#if canImport(UIKit)
+import SwiftUI
+@_exported import UIKit.UIVisualEffect
+import UIKit
+
+public struct VisualEffectView: UIViewRepresentable {
+    public let effect: UIVisualEffect
+    
+    public func makeUIView(context: Context) -> UIVisualEffectView {
+        let view = UIVisualEffectView(effect: self.effect)
+        return view
+    }
+    
+    public func updateUIView(_ effectView: UIVisualEffectView, context: Context) {
+        effectView.effect = effect
+        
+    }
+    
+    public init(effect: UIVisualEffect) {
+        self.effect = effect
+        
+    }
+    
+}
+
+#endif
